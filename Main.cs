4fd83ef83c3759e4dda70ed95c6797bfd3948c2a
Nick Fox <66554932+NickFox007@users.noreply.github.com>
2024-06-23 23:24:54 +0300

    1.1

--- Main.cs
+++ Main.cs
@@ -5,15 +5,19 @@ using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using static VipCoreApi.IVipCoreApi;
 using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using Microsoft.Extensions.Logging;
+using System.Numerics;
 
 
 namespace VipHooks;
 public class VipHooks(): BasePlugin
 {
+    
 
     public override string ModuleAuthor => "Nick Fox";
     public override string ModuleName => "[VIP] Hooks";
-    public override string ModuleVersion => "1.0";
+    public override string ModuleVersion => "1.1";
 
     private IVipCoreApi? _vip;
     private IHooksApi? _hooks;
@@ -38,6 +42,36 @@ public class VipHooks(): BasePlugin
     }
 
 
+    private int[] hooksCount = new int[65];
+    [GameEventHandler]
+    public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        for (int i = 0; i < 65; i++)
+            hooksCount[i] = 0;
+        return HookResult.Continue;
+    }
+
+
+    [GameEventHandler]
+    public HookResult OnFreezeEnd(EventRoundFreezeEnd @event, GameEventInfo info)
+    {
+        for (int i = 0; i < 65; i++)
+        {
+            var player = Utilities.GetPlayerFromSlot(i);
+
+            if (IsValidPlayer(player) && _vip.IsClientVip(player) && _vip.PlayerHasFeature(player, "Hooks"))
+            {
+                var count = _vip.GetFeatureValue<int>(player, "Hooks");
+                if(count == 0)
+                    hooksCount[i] = -1;
+                else
+                    hooksCount[i] = count;
+            }
+        }
+        return HookResult.Continue;
+    }
+
+
     private void OnCoreLoaded()
     {
         _viphooks = new HooksModule(_vip);
@@ -45,11 +79,28 @@ public class VipHooks(): BasePlugin
 
         _hooks.AddHook((playerinfo) => {
 
-            if (_viphooks.HasRights(playerinfo.Player()))
+            var slot = playerinfo.Player().Slot;
+            if (hooksCount[slot] > 0)
+            {
+                hooksCount[slot]--;
+                if (hooksCount[slot] == 0)
+                    _vip.PrintToChat(playerinfo.Player(), Localizer["expired"]);
+                else
+                    _vip.PrintToChat(playerinfo.Player(), String.Format(Localizer["use_count"], hooksCount[slot]));
                 playerinfo.Set(HookState.Enabled);
                 
+            }
+            else 
+                if(hooksCount[slot] == -1)
+                    playerinfo.Set(HookState.Enabled);           
+
         });
 
+    }
+
+    private bool IsValidPlayer(CCSPlayerController player)
+    {
+        return player != null && player.IsValid && player.Connected == PlayerConnectedState.PlayerConnected && !player.IsBot;
     }
 
     public override void Unload(bool hotReload)
@@ -65,15 +116,6 @@ public class VipHooks(): BasePlugin
         public HooksModule(IVipCoreApi api) : base(api)
         {
             
-        }
-
-        public bool HasRights(CCSPlayerController player)
-        {
-            if (IsClientVip(player) && PlayerHasFeature(player))
-            {
-                return true;
-            }
-            else return false;
         }
 
     }
